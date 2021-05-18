57f5c36159ed28ef9a620cdd3d5d049be0c5adc6
yoursunshineR <basdatkubasdatmu@gmail.com>
2021-05-18 14:47:47 +0700

    UPDATE INI
    SASO,FAMEFOW

--- bimbel/FormCoba.Designer.cs
+++ bimbel/FormCoba.Designer.cs
@@ -36,7 +36,7 @@ namespace bimbel
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(-95, 96);
+            this.label1.Location = new System.Drawing.Point(-38, 152);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(1352, 221);
             this.label1.TabIndex = 0;
