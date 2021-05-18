4f03a7d811bb4fb5fc5ed5a8f5416143a74da846
yoursunshineR <basdatkubasdatmu@gmail.com>
2021-05-18 14:43:32 +0700

    ICA

--- bimbel/Program.cs
+++ bimbel/Program.cs
@@ -17,7 +17,7 @@ namespace bimbel
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormCoba());
         }
     }
 }
