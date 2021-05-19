536153ccf9c2aacdb1ba8038d4549fa16a5ba4f3
basdatkubasdatmu <84372990+basdatkubasdatmu@users.noreply.github.com>
2021-05-19 14:28:10 +0800

    Form input nilai - Zia

---
+++ bimbel/FormInputNilai.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace bimbel
+{
+    public partial class FormInputNilai : Form
+    {
+        public FormInputNilai()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNoPengajar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
