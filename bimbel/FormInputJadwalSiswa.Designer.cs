834e9801da24690bce5df442160530d2ff178002
basdatkubasdatmu <basdatkubasdatmu@gmail.com>
2021-05-19 19:10:19 +0700

    TambahForm.Ria

---
+++ bimbel/FormInputJadwalSiswa.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace bimbel
+{
+    partial class FormInputJadwalSiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNoSiswa = new System.Windows.Forms.TextBox();
+            this.lblNoSiswa = new System.Windows.Forms.Label();
+            this.lblKodeJadwalSiswa = new System.Windows.Forms.Label();
+            this.txtKodeJadwalSiswa = new System.Windows.Forms.TextBox();
+            this.btnBatal = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.lblInputJadwalSiswa = new System.Windows.Forms.Label();
+            this.lblKodeJadwalPengajar = new System.Windows.Forms.Label();
+            this.txtKodeJadwalPengajar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNoSiswa
+            // 
+            this.txtNoSiswa.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNoSiswa.Location = new System.Drawing.Point(201, 87);
+            this.txtNoSiswa.Name = "txtNoSiswa";
+            this.txtNoSiswa.Size = new System.Drawing.Size(224, 26);
+            this.txtNoSiswa.TabIndex = 39;
+            // 
+            // lblNoSiswa
+            // 
+            this.lblNoSiswa.AutoSize = true;
+            this.lblNoSiswa.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNoSiswa.Location = new System.Drawing.Point(13, 90);
+            this.lblNoSiswa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNoSiswa.Name = "lblNoSiswa";
+            this.lblNoSiswa.Size = new System.Drawing.Size(69, 20);
+            this.lblNoSiswa.TabIndex = 38;
+            this.lblNoSiswa.Text = "No. Siswa";
+            // 
+            // lblKodeJadwalSiswa
+            // 
+            this.lblKodeJadwalSiswa.AutoSize = true;
+            this.lblKodeJadwalSiswa.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblKodeJadwalSiswa.Location = new System.Drawing.Point(13, 53);
+            this.lblKodeJadwalSiswa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKodeJadwalSiswa.Name = "lblKodeJadwalSiswa";
+            this.lblKodeJadwalSiswa.Size = new System.Drawing.Size(126, 20);
+            this.lblKodeJadwalSiswa.TabIndex = 37;
+            this.lblKodeJadwalSiswa.Text = "Kode Jadwal Siswa";
+            // 
+            // txtKodeJadwalSiswa
+            // 
+            this.txtKodeJadwalSiswa.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtKodeJadwalSiswa.Location = new System.Drawing.Point(201, 50);
+            this.txtKodeJadwalSiswa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKodeJadwalSiswa.Name = "txtKodeJadwalSiswa";
+            this.txtKodeJadwalSiswa.Size = new System.Drawing.Size(224, 26);
+            this.txtKodeJadwalSiswa.TabIndex = 36;
+            // 
+            // btnBatal
+            // 
+            this.btnBatal.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBatal.Location = new System.Drawing.Point(261, 171);
+            this.btnBatal.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBatal.Name = "btnBatal";
+            this.btnBatal.Size = new System.Drawing.Size(78, 39);
+            this.btnBatal.TabIndex = 35;
+            this.btnBatal.Text = "BATAL";
+            this.btnBatal.UseVisualStyleBackColor = true;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSimpan.Location = new System.Drawing.Point(347, 171);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(78, 39);
+            this.btnSimpan.TabIndex = 34;
+            this.btnSimpan.Text = "SIMPAN";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // lblInputJadwalSiswa
+            // 
+            this.lblInputJadwalSiswa.AutoSize = true;
+            this.lblInputJadwalSiswa.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblInputJadwalSiswa.Location = new System.Drawing.Point(13, 9);
+            this.lblInputJadwalSiswa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInputJadwalSiswa.Name = "lblInputJadwalSiswa";
+            this.lblInputJadwalSiswa.Size = new System.Drawing.Size(191, 29);
+            this.lblInputJadwalSiswa.TabIndex = 33;
+            this.lblInputJadwalSiswa.Text = "Input Jadwal Siswa";
+            // 
+            // lblKodeJadwalPengajar
+            // 
+            this.lblKodeJadwalPengajar.AutoSize = true;
+            this.lblKodeJadwalPengajar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblKodeJadwalPengajar.Location = new System.Drawing.Point(13, 128);
+            this.lblKodeJadwalPengajar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKodeJadwalPengajar.Name = "lblKodeJadwalPengajar";
+            this.lblKodeJadwalPengajar.Size = new System.Drawing.Size(144, 20);
+            this.lblKodeJadwalPengajar.TabIndex = 41;
+            this.lblKodeJadwalPengajar.Text = "Kode Jadwal Pengajar";
+            // 
+            // txtKodeJadwalPengajar
+            // 
+            this.txtKodeJadwalPengajar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtKodeJadwalPengajar.Location = new System.Drawing.Point(201, 125);
+            this.txtKodeJadwalPengajar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKodeJadwalPengajar.Name = "txtKodeJadwalPengajar";
+            this.txtKodeJadwalPengajar.Size = new System.Drawing.Size(224, 26);
+            this.txtKodeJadwalPengajar.TabIndex = 40;
+            // 
+            // FormInputJadwalSiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 229);
+            this.Controls.Add(this.lblKodeJadwalPengajar);
+            this.Controls.Add(this.txtKodeJadwalPengajar);
+            this.Controls.Add(this.txtNoSiswa);
+            this.Controls.Add(this.lblNoSiswa);
+            this.Controls.Add(this.lblKodeJadwalSiswa);
+            this.Controls.Add(this.txtKodeJadwalSiswa);
+            this.Controls.Add(this.btnBatal);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.lblInputJadwalSiswa);
+            this.Name = "FormInputJadwalSiswa";
+            this.Text = "Input Jadwal Siswa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNoSiswa;
+        private System.Windows.Forms.Label lblNoSiswa;
+        private System.Windows.Forms.Label lblKodeJadwalSiswa;
+        private System.Windows.Forms.TextBox txtKodeJadwalSiswa;
+        private System.Windows.Forms.Button btnBatal;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Label lblInputJadwalSiswa;
+        private System.Windows.Forms.Label lblKodeJadwalPengajar;
+        private System.Windows.Forms.TextBox txtKodeJadwalPengajar;
+    }
+}
---
+++ bimbel/FormJadwalSiswa.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace bimbel
+{
+    partial class FormJadwalSiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvJadwalSiswa = new System.Windows.Forms.DataGridView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblDaftarJadwalSiswa = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJadwalSiswa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvJadwalSiswa
+            // 
+            this.dgvJadwalSiswa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJadwalSiswa.Location = new System.Drawing.Point(42, 51);
+            this.dgvJadwalSiswa.Name = "dgvJadwalSiswa";
+            this.dgvJadwalSiswa.RowTemplate.Height = 25;
+            this.dgvJadwalSiswa.Size = new System.Drawing.Size(717, 339);
+            this.dgvJadwalSiswa.TabIndex = 20;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnTambah.Location = new System.Drawing.Point(573, 396);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(90, 44);
+            this.btnTambah.TabIndex = 19;
+            this.btnTambah.Text = "TAMBAH";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnHapus.Location = new System.Drawing.Point(669, 396);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(90, 44);
+            this.btnHapus.TabIndex = 18;
+            this.btnHapus.Text = "HAPUS";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRefresh.Location = new System.Drawing.Point(381, 396);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(90, 44);
+            this.btnRefresh.TabIndex = 17;
+            this.btnRefresh.Text = "REFRESH";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEdit.Location = new System.Drawing.Point(477, 396);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(90, 44);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // lblDaftarJadwalSiswa
+            // 
+            this.lblDaftarJadwalSiswa.AutoSize = true;
+            this.lblDaftarJadwalSiswa.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDaftarJadwalSiswa.Location = new System.Drawing.Point(42, 11);
+            this.lblDaftarJadwalSiswa.Name = "lblDaftarJadwalSiswa";
+            this.lblDaftarJadwalSiswa.Size = new System.Drawing.Size(275, 37);
+            this.lblDaftarJadwalSiswa.TabIndex = 15;
+            this.lblDaftarJadwalSiswa.Text = "Daftar Jadwal Siswa";
+            // 
+            // FormJadwalSiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvJadwalSiswa);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lblDaftarJadwalSiswa);
+            this.Name = "FormJadwalSiswa";
+            this.Text = "Jadwal Siswa";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJadwalSiswa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvJadwalSiswa;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblDaftarJadwalSiswa;
+    }
+}
