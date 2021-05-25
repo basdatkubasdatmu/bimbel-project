9436d31218ef5034029121606ceef96997578038
basdatkubasdatmu <84372990+basdatkubasdatmu@users.noreply.github.com>
2021-05-26 00:49:44 +0800

    --ok

--- bimbel/FormMatPel.Designer.cs
+++ bimbel/FormMatPel.Designer.cs
@@ -41,10 +41,10 @@ namespace bimbel
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Candara", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 31);
+            this.label1.Size = new System.Drawing.Size(167, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mata Pelajaran";
             // 
@@ -59,7 +59,7 @@ namespace bimbel
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(508, 332);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 44);
@@ -69,7 +69,7 @@ namespace bimbel
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button4.Location = new System.Drawing.Point(604, 332);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(90, 44);
@@ -80,7 +80,7 @@ namespace bimbel
             // 
             // btRefresh
             // 
-            this.btRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btRefresh.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btRefresh.Location = new System.Drawing.Point(302, 332);
             this.btRefresh.Name = "btRefresh";
             this.btRefresh.Size = new System.Drawing.Size(104, 44);
@@ -91,7 +91,7 @@ namespace bimbel
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(412, 332);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 44);
