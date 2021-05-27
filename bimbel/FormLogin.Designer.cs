e2fb87bd1383ffb7487e1c7bab3419087b84275c
yoursunshineR <basdatkubasdatmu@gmail.com>
2021-05-27 12:18:04 +0700

    add login and change parentform - sunu

--- bimbel/FormLogin.Designer.cs
+++ bimbel/FormLogin.Designer.cs
@@ -29,36 +29,38 @@ namespace bimbel
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLoginSiswa = new System.Windows.Forms.Button();
             this.login = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
-            this.mtxPassword = new System.Windows.Forms.MaskedTextBox();
+            this.txtPassword = new System.Windows.Forms.MaskedTextBox();
             this.userName = new System.Windows.Forms.Label();
             this.pwLogin = new System.Windows.Forms.Label();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btnLoginPegawai = new System.Windows.Forms.Button();
+            this.txtPasswordPgw = new System.Windows.Forms.MaskedTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUsernamePgw = new System.Windows.Forms.TextBox();
             this.login.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnLogin
+            // btnLoginSiswa
             // 
-            this.btnLogin.BackColor = System.Drawing.SystemColors.Info;
-            this.btnLogin.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLogin.Location = new System.Drawing.Point(236, 159);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(88, 34);
-            this.btnLogin.TabIndex = 13;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLoginSiswa.BackColor = System.Drawing.SystemColors.Info;
+            this.btnLoginSiswa.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLoginSiswa.Location = new System.Drawing.Point(270, 212);
+            this.btnLoginSiswa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLoginSiswa.Name = "btnLoginSiswa";
+            this.btnLoginSiswa.Size = new System.Drawing.Size(101, 45);
+            this.btnLoginSiswa.TabIndex = 13;
+            this.btnLoginSiswa.Text = "Login";
+            this.btnLoginSiswa.UseVisualStyleBackColor = false;
+            this.btnLoginSiswa.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // login
             // 
@@ -66,9 +68,10 @@ namespace bimbel
             this.login.Controls.Add(this.tabPage1);
             this.login.Controls.Add(this.tabPage2);
             this.login.Location = new System.Drawing.Point(1, -1);
+            this.login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.login.Name = "login";
             this.login.SelectedIndex = 0;
-            this.login.Size = new System.Drawing.Size(369, 241);
+            this.login.Size = new System.Drawing.Size(422, 321);
             this.login.TabIndex = 15;
             this.login.Tag = "";
             this.login.SelectedIndexChanged += new System.EventHandler(this.siswa_SelectedIndexChanged);
@@ -77,16 +80,17 @@ namespace bimbel
             // 
             this.tabPage1.BackColor = System.Drawing.Color.AliceBlue;
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.btnLogin);
-            this.tabPage1.Controls.Add(this.mtxPassword);
+            this.tabPage1.Controls.Add(this.btnLoginSiswa);
+            this.tabPage1.Controls.Add(this.txtPassword);
             this.tabPage1.Controls.Add(this.userName);
             this.tabPage1.Controls.Add(this.pwLogin);
             this.tabPage1.Controls.Add(this.txtUsername);
             this.tabPage1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(361, 213);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(414, 288);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Siswa";
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
@@ -95,27 +99,28 @@ namespace bimbel
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Location = new System.Drawing.Point(8, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 33);
+            this.label1.Size = new System.Drawing.Size(100, 41);
             this.label1.TabIndex = 1;
             this.label1.Text = "Login";
             // 
-            // mtxPassword
+            // txtPassword
             // 
-            this.mtxPassword.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.mtxPassword.Location = new System.Drawing.Point(116, 109);
-            this.mtxPassword.Name = "mtxPassword";
-            this.mtxPassword.Size = new System.Drawing.Size(208, 27);
-            this.mtxPassword.TabIndex = 14;
+            this.txtPassword.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPassword.Location = new System.Drawing.Point(133, 145);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(237, 32);
+            this.txtPassword.TabIndex = 14;
             // 
             // userName
             // 
             this.userName.AutoSize = true;
             this.userName.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.userName.Location = new System.Drawing.Point(34, 65);
+            this.userName.Location = new System.Drawing.Point(39, 87);
             this.userName.Name = "userName";
-            this.userName.Size = new System.Drawing.Size(79, 19);
+            this.userName.Size = new System.Drawing.Size(96, 24);
             this.userName.TabIndex = 7;
             this.userName.Text = "Username";
             // 
@@ -123,33 +128,35 @@ namespace bimbel
             // 
             this.pwLogin.AutoSize = true;
             this.pwLogin.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.pwLogin.Location = new System.Drawing.Point(34, 112);
+            this.pwLogin.Location = new System.Drawing.Point(39, 149);
             this.pwLogin.Name = "pwLogin";
-            this.pwLogin.Size = new System.Drawing.Size(76, 19);
+            this.pwLogin.Size = new System.Drawing.Size(91, 24);
             this.pwLogin.TabIndex = 9;
             this.pwLogin.Text = "Password";
             // 
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtUsername.Location = new System.Drawing.Point(116, 62);
+            this.txtUsername.Location = new System.Drawing.Point(133, 83);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(208, 27);
+            this.txtUsername.Size = new System.Drawing.Size(237, 32);
             this.txtUsername.TabIndex = 8;
             // 
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.Lavender;
             this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Controls.Add(this.maskedTextBox1);
+            this.tabPage2.Controls.Add(this.btnLoginPegawai);
+            this.tabPage2.Controls.Add(this.txtPasswordPgw);
             this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.textBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Controls.Add(this.txtUsernamePgw);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(361, 213);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Size = new System.Drawing.Size(414, 288);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Pegawai/Pengajar";
             // 
@@ -157,38 +164,41 @@ namespace bimbel
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(7, 10);
+            this.label2.Location = new System.Drawing.Point(8, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 33);
+            this.label2.Size = new System.Drawing.Size(100, 41);
             this.label2.TabIndex = 15;
             this.label2.Text = "Login";
             // 
-            // button1
+            // btnLoginPegawai
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Info;
-            this.button1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(239, 163);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 30);
-            this.button1.TabIndex = 19;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnLoginPegawai.BackColor = System.Drawing.SystemColors.Info;
+            this.btnLoginPegawai.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLoginPegawai.Location = new System.Drawing.Point(273, 217);
+            this.btnLoginPegawai.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLoginPegawai.Name = "btnLoginPegawai";
+            this.btnLoginPegawai.Size = new System.Drawing.Size(101, 40);
+            this.btnLoginPegawai.TabIndex = 19;
+            this.btnLoginPegawai.Text = "Login";
+            this.btnLoginPegawai.UseVisualStyleBackColor = false;
+            this.btnLoginPegawai.Click += new System.EventHandler(this.button1_Click);
             // 
-            // maskedTextBox1
+            // txtPasswordPgw
             // 
-            this.maskedTextBox1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.maskedTextBox1.Location = new System.Drawing.Point(119, 109);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(208, 27);
-            this.maskedTextBox1.TabIndex = 20;
+            this.txtPasswordPgw.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPasswordPgw.Location = new System.Drawing.Point(136, 145);
+            this.txtPasswordPgw.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPasswordPgw.Name = "txtPasswordPgw";
+            this.txtPasswordPgw.Size = new System.Drawing.Size(237, 32);
+            this.txtPasswordPgw.TabIndex = 20;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(30, 66);
+            this.label3.Location = new System.Drawing.Point(34, 88);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 19);
+            this.label3.Size = new System.Drawing.Size(96, 24);
             this.label3.TabIndex = 16;
             this.label3.Text = "Username";
             // 
@@ -196,28 +206,30 @@ namespace bimbel
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(30, 113);
+            this.label4.Location = new System.Drawing.Point(34, 151);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 19);
+            this.label4.Size = new System.Drawing.Size(91, 24);
             this.label4.TabIndex = 18;
             this.label4.Text = "Password";
             // 
-            // textBox1
+            // txtUsernamePgw
             // 
-            this.textBox1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(119, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(208, 27);
-            this.textBox1.TabIndex = 17;
+            this.txtUsernamePgw.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUsernamePgw.Location = new System.Drawing.Point(136, 83);
+            this.txtUsernamePgw.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUsernamePgw.Name = "txtUsernamePgw";
+            this.txtUsernamePgw.Size = new System.Drawing.Size(237, 32);
+            this.txtUsernamePgw.TabIndex = 17;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(369, 239);
+            this.ClientSize = new System.Drawing.Size(422, 319);
             this.Controls.Add(this.login);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormLogin";
@@ -232,20 +244,20 @@ namespace bimbel
         }
 
         #endregion
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLoginSiswa;
         private System.Windows.Forms.TabControl login;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.MaskedTextBox mtxPassword;
+        private System.Windows.Forms.MaskedTextBox txtPassword;
         private System.Windows.Forms.Label userName;
         private System.Windows.Forms.Label pwLogin;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button btnLoginPegawai;
+        private System.Windows.Forms.MaskedTextBox txtPasswordPgw;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUsernamePgw;
     }
 }
--- bimbel/FormLogin.cs
+++ bimbel/FormLogin.cs
@@ -10,6 +10,8 @@ namespace bimbel
 {
     public partial class FormLogin : Form
     {
+        
+
         public FormLogin()
         {
             InitializeComponent();
@@ -43,6 +45,82 @@ namespace bimbel
         private void siswa_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        public string noSiswaLogin;
+
+        public Boolean isSiswa;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "" && txtPassword.Text != "")
+            {
+                DataAccess da = new DataAccess();
+                DataTable dataSiswa = da.getSiswaByID(txtUsername.Text); //Msalkan loginnya pakai ID pegawai (bisa juga buat kolom username sendiri di database)
+
+                if (dataSiswa.Rows.Count == 0)
+                {
+                    MessageBox.Show("Pegawai tersebut tidak terdaftar", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    //Jika ada pegawai tersebut di tabel pegawai
+                    //Cek apakah password-nya benar
+                    if (txtPassword.Text == dataSiswa.Rows[0]["nohp"].ToString()) //Misalnya passwordnya pakai nohp (bisa buat kolom password sendiri)
+                    {
+                        this.noSiswaLogin = txtUsername.Text;
+                        this.DialogResult = DialogResult.OK;
+                        this.isSiswa = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password salah", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silakan masukan username dan password terlebih dahulu", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public string noPegawaiLogin;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsernamePgw.Text != "" && txtPasswordPgw.Text != "")
+            {
+                DataAccess da = new DataAccess();
+                DataTable dataPgw = da.getDataPegawaiByID(txtUsernamePgw.Text); //Msalkan loginnya pakai ID pegawai (bisa juga buat kolom username sendiri di database)
+
+                if (dataPgw.Rows.Count == 0)
+                {
+                    MessageBox.Show("Pegawai tersebut tidak terdaftar", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    //Jika ada pegawai tersebut di tabel pegawai
+                    //Cek apakah password-nya benar
+                    if (txtPasswordPgw.Text == dataPgw.Rows[0]["nohp"].ToString()) //Misalnya passwordnya pakai nohp (bisa buat kolom password sendiri)
+                    {
+                        this.noPegawaiLogin = txtUsernamePgw.Text;
+                        this.DialogResult = DialogResult.OK;
+                        this.isSiswa = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password salah", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silakan masukan username dan password terlebih dahulu", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
